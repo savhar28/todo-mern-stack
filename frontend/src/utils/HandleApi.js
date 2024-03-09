a9b58ea30e820b89674d524998bf7f1bc431c989
savhar28 <savharverma@gmail.com>
2024-03-09 22:12:25 +0530

    frontend URL

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://todo-mern-stack-cvqj.onrender.com";
 
 const getAllToDo = (setToDo) => {
     axios
         .get(baseUrl)
-        
+
         .then(({data}) => {
             console.log('data ---->', data);
             setToDo(data);
